277504e64b2c5610b298e1526fe62709feabfe5e
Ira Greenberg <irajgreenberg@gmail.com>
2012-11-14 09:49:51 -0600

    old stuff

---
+++ src/SubdivisionSurf.cpp
@@ -0,0 +1,9 @@
+//
+//  SubdivisionSurf.cpp
+//  Protobyte_iig001
+//
+//  Created by Ira on 11/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
---
+++ src/SubdivisionSurf.h
@@ -0,0 +1,14 @@
+//
+//  SubdivisionSurf.h
+//  Protobyte_iig001
+//
+//  Created by Ira on 11/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef Protobyte_iig001_SubdivisionSurf_h
+#define Protobyte_iig001_SubdivisionSurf_h
+
+
+
+#endif
